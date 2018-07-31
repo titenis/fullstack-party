2c6d65b8af2b2c4ca098f7f16bead08197a2fd60
zilvinas titenis <zilvinas@titenis.lt>
2018-08-01 00:01:14 +0300

    first two html pages done

---
+++ ui/src/app/main.js
@@ -0,0 +1,3 @@
+import '../scss/main.scss';
+import '../scss/login.scss';
+import '../scss/list.scss';
--- ui/webpack.config.js
+++ ui/webpack.config.js
@@ -3,19 +3,56 @@ const webpack = require('webpack');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = (env) => {
+module.exports = (mode) => {
 
-    const _DEV_ = env === 'development';
+    const _DEV_ = mode === 'development';
 
     return {
         context: path.resolve(__dirname),
         entry: ['./src/app/main.js'],
         output: {
             path: path.resolve(__dirname, './dist'),
+            filename: 'bundle.js',
             publicPath: '/'
         },
-        module: {},
+        module: {
+            rules: [
+                {
+                    test: /\.(sa|sc|c)ss$/,
+                    use: [
+                        _DEV_ ? 'style-loader' : MiniCssExtractPlugin.loader,
+                        'css-loader',
+                        {
+                            loader: 'postcss-loader',
+                            options: {
+                                plugins: () => ([
+                                    require('precss'),
+                                    require('autoprefixer')
+                                ])
+                            }
+                        },
+                        'sass-loader',
+                    ],
+                },
+                // {test: /\.scss$/, use: [
+                //     {loader: 'style-loader'},
+                //     {loader: 'css-loader'},
+                //     {
+                //         loader: 'postcss-loader',
+                //         options: {
+                //             plugins: () => ([
+                //                 require('precss'),
+                //                 require('autoprefixer')
+                //             ])
+                //         }
+                //     },
+                //     {loader: 'sass-loader'}
+                // ]},
+                {test: /\.(jpe?g|png|gif)$/, loader: 'file-loader', query: {name: 'assets/img/[name].[ext]'}},
+            ]
+        },
         devServer: {
             contentBase: path.join(__dirname, "dist"),
             compress: true,
@@ -32,15 +69,27 @@ module.exports = (env) => {
             poll: true,
             ignored: '/node_modules/'
         },
-
         plugins: [
             new CleanWebpackPlugin(['dist']),
             new CopyWebpackPlugin([
+                {from: './src/html'},
                 {from: './src/index.html'},
                 {from: './src/assets', to: './assets'}
             ]),
             new HtmlWebpackPlugin({
                 template: path.resolve(__dirname, 'src/index.html')
+            }),
+            new HtmlWebpackPlugin({
+                filename: 'login.html',
+                template: path.resolve(__dirname, 'src/html/login.html')
+            }),
+            new HtmlWebpackPlugin({
+                filename: 'list.html',
+                template: path.resolve(__dirname, 'src/html/list.html')
+            }),
+            new MiniCssExtractPlugin({
+                filename: "[name].css",
+                chunkFilename: "[id].css"
             })
         ].concat(_DEV_
             ? [
